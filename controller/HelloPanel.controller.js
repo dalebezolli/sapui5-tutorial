292c7ed631d01976f6c11edd4547ed2c15bc672d
Dale Bezolli <deilbezolli@gmail.com>
2023-07-12 14:52:25 +0000

    Create dialog fragment

--- controller/HelloPanel.controller.js
+++ controller/HelloPanel.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
     'sap/ui/core/mvc/Controller',
     'sap/m/MessageToast',
-], function(Controller, MessageToast) {
+    'sap/ui/core/Fragment'
+], function(Controller, MessageToast, Fragment) {
 
     return Controller.extend('sap.ui.demo.walkthrough.controller.HelloPanel', {
         onShowHello: function() {
@@ -10,6 +11,15 @@ sap.ui.define([
             const message = i18Model.getText('helloMsg', recipient);
         
             MessageToast.show(message);
+        },
+        onShowDialogHello: function() {
+            const recipient = this.getView().getModel().getProperty('/recipient/name');
+        
+            if(!this.pDialog) {
+                this.pDialog = this.loadFragment({ name: 'sap.ui.demo.walkthrough.view.HelloDialog'});
+            }
+
+            this.pDialog.then(function(oDialog) { oDialog.open() });
         }
     });
 });
