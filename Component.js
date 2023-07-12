f569a34bb1ce2d994dbde044a7c6183a39ec4160
Dale Bezolli <deilbezolli@gmail.com>
2023-07-12 13:28:20 +0000

    Decouple Component Logic from index

---
+++ Component.js
@@ -0,0 +1,30 @@
+sap.ui.define([ 
+    'sap/ui/core/UIComponent',
+    'sap/ui/model/json/JSONModel',
+    'sap/ui/model/resource/ResourceModel',
+], function(UIComponent, JSONModel, ResourceModel) {
+    'use strict';
+
+    return UIComponent.extend('sap.ui.demo.walkthrough.Component', {
+        metadata: {
+            'interfaces': 'sap.ui.core.IAsyncContentCreation',
+            'rootView': {
+                'viewName': 'sap.ui.demo.walkthrough.view.App',
+                'type': 'XML',
+                'id': 'app'
+            }
+        },
+        init: function() {
+            UIComponent.prototype.init.apply(this, arguments);
+
+            const oModel = new JSONModel({ recipient: { name: 'World' }});
+            this.setModel(oModel);
+
+            const i18nModel = new ResourceModel({
+                bundleName: 'sap.ui.demo.walkthrough.i18n.i18n'
+            });
+
+            this.setModel(i18nModel, 'i18n');
+        }
+    });
+});
--- controller/App.controller.js
+++ controller/App.controller.js
@@ -1,22 +1,10 @@
 sap.ui.define([
     'sap/ui/core/mvc/Controller',
     'sap/m/MessageToast',
-    'sap/ui/model/json/JSONModel',
-    'sap/ui/model/resource/ResourceModel'
-], function(Controller, MessageToast, JSONModel, ResourceModel) {
+], function(Controller, MessageToast) {
     'use strict';
 
     return Controller.extend('sap.ui.demo.walkthrough.controller.App', {
-        onInit: function() {
-            const oModel = new JSONModel({ recipient: { name: 'World' }});
-            this.getView().setModel(oModel);
-
-            const i18nModel = new ResourceModel({
-                bundleName: 'sap.ui.demo.walkthrough.i18n.i18n'
-            });
-
-            this.getView().setModel(i18nModel, 'i18n');
-        },
         onShowHello: function() {
             const recipient = this.getView().getModel().getProperty('/recipient/name');
             const i18nModel = this.getView().getModel('i18n').getResourceBundle();
